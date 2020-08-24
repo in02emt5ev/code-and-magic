576cf59fc67e2855a0dd2055ad568cb8c3b9b815
in02emt5ev <in02emt5ev@yandex.ru>
2020-08-24 23:49:56 +0700

    Вводит данные для похожих волшебников

--- js/setup.js
+++ js/setup.js
@@ -3,3 +3,42 @@
 var setup = document.querySelector('.setup');
 
 setup.classList.remove('hidden');
+
+// Создание похожих волшебников;
+
+var wizardNames = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
+];
+var wizardSurnames = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг'
+];
+var wizardCoatColors = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)',
+];
+var wizardEyesColors = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+];
+
